9d3d2385925b88bc7879e5bcb3a9abe715514545
Ornii_ <111347574+Ornii@users.noreply.github.com>
2025-12-26 23:28:10 +0100

    Update config path handling in main.py

--- client/main.py
+++ client/main.py
@@ -2,9 +2,11 @@ from config.config import AppConfig
 from led.state import LedState
 from network.tcp import TcpClient
 
+BASE_DIR = str(Path(__file__).parent.resolve())
+CONFIG_PATH = BASE_DIR + r"\config.yaml"
 
 def main():
-    config = AppConfig.load_from_yaml("config.yaml")
+    config = AppConfig.load_from_yaml(CONFIG_PATH)
     led_state = LedState()
     tcp_client = TcpClient(led_state)
     tcp_client.start_connection(config.tcp_server_addr, config.tcp_port)
@@ -12,4 +14,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
