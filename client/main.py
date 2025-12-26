4312a41f62cc71bbc07f3e96ac0d05557bceb68e
Ornii_ <111347574+Ornii@users.noreply.github.com>
2025-12-26 23:05:21 +0100

    Refactor TcpClient initialization in main.py

--- client/main.py
+++ client/main.py
@@ -6,9 +6,10 @@ from network.tcp import TcpClient
 def main():
     config = AppConfig.load_from_yaml("config.yaml")
     led_state = LedState()
-    tcp_client = TcpClient(led_state, config.tcp_port)
+    tcp_client = TcpClient(led_state)
     tcp_client.start_connection(config.tcp_server_addr, config.tcp_port)
 
 
 if __name__ == "__main__":
     main()
+
